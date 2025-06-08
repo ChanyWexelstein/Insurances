bc75547b4064c9aba6f1a4603100313794aab7c4
HadassahGottfried <hadasa3281@gmail.com>
2025-06-08 12:05:34 +0300

    Project infrastructure upload.

---
+++ Insurances-backend/Models/NewInquiry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Insurances_backend.Models;
+
+public partial class NewInquiry
+{
+    public int Id { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public double SalaryAmount { get; set; }
+
+    public int StatusCode { get; set; }
+
+    public virtual Status StatusCodeNavigation { get; set; } = null!;
+}
